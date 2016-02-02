896dd92acaa2bc5dfd2a6b9f389f0e013fb04f7f
Jesse Vincent <jesse@bestpractical.com>
2016-02-01 21:37:02 -0800

    Beginnings of implementation of the new backend

--- implementation/Model01.cpp
+++ implementation/Model01.cpp
@@ -20,13 +20,24 @@ void Model01::led_sync() {
 
 void Model01::scan_matrix() {
     uint8_t key_data;
-    //scan the Keyboard matrix looking for connections
-    for (byte row = 0; row < 4; row++) {
 
-        for (byte col = 0; col < 4; col++) {
-            handle_key_event(row, col, &key_data);
-            handle_key_event(row, (8- 1) - col, &key_data);
-        }
-    }
+leftHand.sendLEDData();
+// check if a key is ready, and if so, then read it.
+key_t left_key = leftHand.readKey();
+if (left_key.key>0) {
+            handle_key_event(0,0, &key_data);
+}
+
+
+rightHand.sendLEDData();
+// check if a key is ready, and if so, then read it.
+key_t right_key = rightHand.readKey();
+if (right_key.key>0) {
+            handle_key_event(0,0, &key_data);
+}
+
+
+
+
 }
 

bbce7d40afa1c25eafafa6e2ca0109c300893a69
Jesse Vincent <jesse@keyboard.io>
2017-06-03 23:49:57 -0700

    astyle to pad operators

--- src/AtmegaScanner.h
+++ src/AtmegaScanner.h
@@ -22,122 +22,120 @@
 
 template<uint8_t cols_, uint8_t rows_>
 class AtmegaScanner {
-  public:
-    typedef struct {
-        uint32_t all;
-    } keydata_t;
+ public:
+  typedef struct {
+    uint32_t all;
+  } keydata_t;
 
-    AtmegaScanner(void);
+  AtmegaScanner(void);
 
-    bool readKeys(void);
+  bool readKeys(void);
 
-    keydata_t leftHandState;
-    keydata_t rightHandState;
-    keydata_t previousLeftHandState;
-    keydata_t previousRightHandState;
+  keydata_t leftHandState;
+  keydata_t rightHandState;
+  keydata_t previousLeftHandState;
+  keydata_t previousRightHandState;
 
-  private:
-    void initCols(void);
-    void unselectRows(void);
-    void selectRow(uint8_t row);
-    uint16_t readCols(void);
+ private:
+  void initCols(void);
+  void unselectRows(void);
+  void selectRow(uint8_t row);
+  uint16_t readCols(void);
 };
 
 template<uint8_t cols_, uint8_t rows_>
 void AtmegaScanner<cols_, rows_>::unselectRows(void) {
-    DDRD &= ~0b00110000;
-    PORTD &= ~0b00110000;
+  DDRD &= ~0b00110000;
+  PORTD &= ~0b00110000;
 
-    DDRE &= ~0b01000100;
-    PORTE &= ~0b01000100;
+  DDRE &= ~0b01000100;
+  PORTE &= ~0b01000100;
 }
 
 template<uint8_t cols_, uint8_t rows_>
 void AtmegaScanner<cols_, rows_>::initCols(void) {
-    DDRB &= ~(1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
-    PORTB |= (1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
-    DDRC &= ~(1<<7 | 1<<6);
-    PORTC |= (1<<7 | 1<<6);
-    DDRD &= ~(1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<7);
-    PORTD |= (1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<7);
-    DDRF &= ~(1<<6 | 1<<7);
-    PORTF |= (1<<6 | 1<<7);
+  DDRB &= ~(1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
+  PORTB |= (1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
+  DDRC &= ~(1<<7 | 1<<6);
+  PORTC |= (1<<7 | 1<<6);
+  DDRD &= ~(1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<7);
+  PORTD |= (1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<7);
+  DDRF &= ~(1<<6 | 1<<7);
+  PORTF |= (1<<6 | 1<<7);
 }
 
 template<uint8_t cols_, uint8_t rows_>
-void AtmegaScanner<cols_, rows_>::selectRow(uint8_t row)
-{
-    switch (row) {
-    case 0:
-        DDRD  |= (1<<4);
-        PORTD &= ~(1<<4);
-        break;
-    case 1:
-        DDRD  |= (1<<5);
-        PORTD &= ~(1<<5);
-        break;
-    case 2:
-        DDRE  |= (1<<2);
-        PORTE &= ~(1<<2);
-        break;
-    case 3:
-        DDRE  |= (1<<6);
-        PORTE &= ~(1<<6);
-        break;
-    default:
-        break;
-    }
+void AtmegaScanner<cols_, rows_>::selectRow(uint8_t row) {
+  switch (row) {
+  case 0:
+    DDRD  |= (1<<4);
+    PORTD &= ~(1<<4);
+    break;
+  case 1:
+    DDRD  |= (1<<5);
+    PORTD &= ~(1<<5);
+    break;
+  case 2:
+    DDRE  |= (1<<2);
+    PORTE &= ~(1<<2);
+    break;
+  case 3:
+    DDRE  |= (1<<6);
+    PORTE &= ~(1<<6);
+    break;
+  default:
+    break;
+  }
 }
 
 template<uint8_t cols_, uint8_t rows_>
-uint16_t AtmegaScanner<cols_, rows_>::readCols(void)
-{
-    return (PINF&(1<<6) ? 0 : (1<<0)) |
-           (PINF&(1<<7) ? 0 : (1<<1)) |
-           (PINB&(1<<4) ? 0 : (1<<2)) |
-           (PINB&(1<<5) ? 0 : (1<<3)) |
-           (PINB&(1<<6) ? 0 : (1<<4)) |
-           (PIND&(1<<7) ? 0 : (1<<5)) |
-           (PINC&(1<<6) ? 0 : (1<<6)) |
-           (PINC&(1<<7) ? 0 : (1<<7)) |
-           (PIND&(1<<3) ? 0 : (1<<8)) |     // Rev.A and B
-           (PIND&(1<<2) ? 0 : (1<<9)) |
-           (PIND&(1<<1) ? 0 : (1<<10)) |
-           (PIND&(1<<0) ? 0 : (1<<11)) |
-           (PINB&(1<<7) ? 0 : (1<<12)) |
-           (PINB&(1<<3) ? 0 : (1<<13));
+uint16_t AtmegaScanner<cols_, rows_>::readCols(void) {
+  return (PINF&(1<<6) ? 0 : (1<<0)) |
+         (PINF&(1<<7) ? 0 : (1<<1)) |
+         (PINB&(1<<4) ? 0 : (1<<2)) |
+         (PINB&(1<<5) ? 0 : (1<<3)) |
+         (PINB&(1<<6) ? 0 : (1<<4)) |
+         (PIND&(1<<7) ? 0 : (1<<5)) |
+         (PINC&(1<<6) ? 0 : (1<<6)) |
+         (PINC&(1<<7) ? 0 : (1<<7)) |
+         (PIND&(1<<3) ? 0 : (1<<8)) |     // Rev.A and B
+         (PIND&(1<<2) ? 0 : (1<<9)) |
+         (PIND&(1<<1) ? 0 : (1<<10)) |
+         (PIND&(1<<0) ? 0 : (1<<11)) |
+         (PINB&(1<<7) ? 0 : (1<<12)) |
+         (PINB&(1<<3) ? 0 : (1<<13));
 }
 
 /* ------------------------------------ */
 
 template<uint8_t cols_, uint8_t rows_>
 AtmegaScanner<cols_, rows_>::AtmegaScanner(void) {
-    leftHandState.all = 0;
-    rightHandState.all = 0;
-    previousLeftHandState.all = 0;
-    previousRightHandState.all = 0;
+  leftHandState.all = 0;
+  rightHandState.all = 0;
+  previousLeftHandState.all = 0;
+  previousRightHandState.all = 0;
 
-    unselectRows();
-    initCols();
+  unselectRows();
+  initCols();
 }
 
 template<uint8_t cols_, uint8_t rows_>
 bool AtmegaScanner<cols_, rows_>::readKeys() {
-    previousLeftHandState = leftHandState;
-    previousRightHandState = rightHandState;
+  previousLeftHandState = leftHandState;
+  previousRightHandState = rightHandState;
 
-    for (uint8_t i = 0; i < rows_; i++) {
-        selectRow(i);
-        uint16_t cols = readCols();
+  for (uint8_t i = 0; i < rows_; i++) {
+    selectRow(i);
+    uint16_t cols = readCols();
 
-        for (uint8_t c = 0; c < cols_ / 2; c++) {
-            uint8_t rowPos = i * (cols_ / 2);
-            bitWrite(leftHandState.all, rowPos + c, bitRead(cols, c));
-            bitWrite(rightHandState.all, rowPos + ((cols_ / 2 - 1) - c), bitRead(cols, (cols_ / 2) + c));
-        }
-
-       unselectRows();
+    for (uint8_t c = 0; c < cols_ / 2; c++) {
+      uint8_t rowPos = i * (cols_ / 2);
+      bitWrite(leftHandState.all, rowPos + c, bitRead(cols, c));
+      bitWrite(rightHandState.all, rowPos + ((cols_ / 2 - 1) - c), bitRead(cols, (cols_ / 2) + c));
     }
 
-    return true;
+    unselectRows();
+  }
+
+  return true;
 }
